89011ccc3536957ae24bb17b93066f1b544a3ae6
sonicskye <sonic_skye@yahoo.com>
2018-11-25 06:50:53 +1100

    optimize the wordlist processing

--- bloomfilter.py
+++ bloomfilter.py
@@ -5,6 +5,10 @@ bloomfilter.py
 Requirements:
 bitarray (https://github.com/ilanschnell/bitarray)
 pybloof (https://github.com/jhgg/pybloof)
+
+Pybloof library is used due to its built-in export and import features
+These features are convenient for storing the bloom filter information to the smart contract
+    and import them if needed
 '''
 
 import pybloof
@@ -13,8 +17,10 @@ import utilities as u
 
 def createstringbloomfilter(wordlist):
     # if the size is too small then the result is inaccurate
-    # makes sure that there is an extra 500 allowance
-    sz = len(wordlist) + 500
+    # makes sure that there is an extra 500 allowance; false positive still exists
+    # the input wordlist is converted into setwordlist to remove duplicates
+    setwordlist = set(wordlist)
+    sz = len(setwordlist) + 500
     bf = pybloof.StringBloomFilter(size=sz, hashes=9)
     for word in wordlist:
         #bf.add(word)
